577c089e279315403e82d878c59d82e8dfdce6ec
eooomji <70639677+eooomji@users.noreply.github.com>
2021-08-03 13:55:50 +0900

    Create 3.1.1.py

---
+++ task/3.1/3.1.1.py
@@ -0,0 +1,11 @@
+# 3.1.1 code
+import numpy as np
+import random
+
+randArray = np.random.randint(0,9,(10,10))
+
+randArrayPlus = randArray + 1
+
+## check
+print(randArray, '\n')
+print(randArrayPlus)
