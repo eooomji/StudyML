8ca0f1bfd913985e9b1ddd0d2e5c6b26529b1115
eooomji <70639677+eooomji@users.noreply.github.com>
2021-08-03 14:11:34 +0900

    shape : 데이터 크기 / isnull() : 결손값

---
+++ code/Chap04/14.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data_loc = 'https://github.com/dknife/ML/raw/main/data/'
+life = pd.read_csv(data_loc + 'life_expectancy.csv')
+# life.head()
+
+### 관심있는 데이터만 추출 + 원하는 열 지정
+life = life[['Life expectancy', 'Year', 'Alcohol', 'Percentage expenditure', 'Total expenditure', 'Hepatitis B', 'Measles', 'Polio', 'BMI', 'GDP', 'Thinness 1-19 years', 'Thinness 5-9 years']]
+# life.head()
+
+# NEW
+### 데이터 크기, 결손값 살펴보기
+print(life.shape)           # shape : 크기 알 수 있음 => 행, 열로 예상 => 데이터 총 2938개 / 데이터 항목 : 12개
+print('------------------------------')
+print(life.isnull().sum())  # isnull() : 결손값 => Year, Percentage expenditure, Measles는 결손값이 존재한다고 판단됨
+print('------------------------------')
+
+### 결손값 있는 데이터 삭제
+life.dropna(inplace = True)
+print('결손값 있는 데이터 삭제 후 남은 데이터 크기 :', life.shape)
+print('------------------------------')
