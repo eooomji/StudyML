40f3eea14aa29bb07b10deed9a7029feecd5857f
eooomji <70639677+eooomji@users.noreply.github.com>
2021-08-03 13:50:47 +0900

    Create 4.3.py

---
+++ code/Chap04/4.3.py
@@ -0,0 +1,9 @@
+# setting
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data_home = 'https://github.com/dknife/ML/raw/main/data/'
+lin_data = pd.read_csv(data_home+'pollution.csv')
+
+print(lin_data)
