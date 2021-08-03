b7e7312c12b027ebb4973b9051d714bda8268346
eooomji <70639677+eooomji@users.noreply.github.com>
2021-08-03 13:56:38 +0900

    Create 3.1.3.py

---
+++ task/3.1/3.1.3.py
@@ -0,0 +1,14 @@
+# 3.1.3
+## 더 나은 방법 생각해보기
+import numpy as np
+import random
+
+randArray = np.random.randint(0,9,(10,10)) + 1
+
+for i in range(0, 10, 2):
+  randArray[i] = 0
+
+for i in range(1, 10, 2):
+  randArray[i][1::2] = 0
+
+randArray
