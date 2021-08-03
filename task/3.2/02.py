af96841027073d4d2db08c7ad663e4c7aaf6c0f0
eooomji <70639677+eooomji@users.noreply.github.com>
2021-08-03 14:01:27 +0900

    name열을 인덱스로 지정

---
+++ task/3.2/02.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+P_carCompany = pd.DataFrame ({'name' :  ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
+                              'horse power' : ['130', '250', '190', '300', '210', '220', '170'],
+                              'weight' : ['1.9', '2.6', '2.2', '2.9', '2.4', '2.3', '2.2'],
+                              'efficiency' : ['16.3', '10.2', '11.1', '7.1', '12.1', '13.2', '14.2']})
+
+P_carCompany.set_index('name')
