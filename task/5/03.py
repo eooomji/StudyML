222a04c2d7b76325eb63d190831351cb92b33ecf
eooomji <70639677+eooomji@users.noreply.github.com>
2021-08-07 17:19:51 +0900

    데이터 A, B, C, D 분류 결과 출력

--- task/5/03.py
+++ task/5/03.py
@@ -1,28 +1,6 @@
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 
-# 닥스훈트
-dach_length = [75, 77, 83, 81, 73, 99, 72, 83]
-dach_height = [24, 29, 19, 32, 21, 22, 19, 34]
-
-# 사모예드
-samo_length = [76, 78, 82, 88, 76, 83, 81, 89]
-samo_height = [55, 58, 53, 54, 61, 52, 57, 64]
-
-# 말티즈
-mal_length = [35, 39, 38, 41, 30, 57, 41, 35]
-mal_height = [23, 26, 19, 30, 21, 24, 28, 20]
-
-# 5.1 레이블링
-d_data = np.column_stack((dach_length, dach_height))
-d_label = np.array([0, 0, 0, 0, 0, 0, 0, 0])
-
-s_data = np.column_stack((samo_length, samo_height))
-s_label = np.array([1, 1, 1, 1, 1, 1, 1, 1])
-
-m_data = np.column_stack((mal_length, mal_height))
-m_label = np.array([2, 2, 2, 2, 2, 2, 2, 2])
-
 dogs = np.concatenate((d_data, s_data, m_data))
 labels = np.concatenate((d_label, s_label, m_label))
 
